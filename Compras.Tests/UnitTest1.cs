16c459cfc03bf9be824032d34d0fc4946865c83f
Ericksson Souza Drozda <fif_ts@hotmail.com>
2018-06-17 00:59:38 -0300

    add unit test

--- Compras.Tests/UnitTest1.cs
+++ Compras.Tests/UnitTest1.cs
@@ -4,6 +4,10 @@ using Compras.Tests.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
+using System.Collections.Specialized;
+using System.Text;
+using System.IO;
 
 namespace Compras.Tests
 {
@@ -14,7 +18,26 @@ namespace Compras.Tests
         [TestMethod]
         public void TestMethod1()
         {
-         
+            //teste POST
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:8084/pessoas");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = "{\"nome\":\"jabuna\"," +
+                              "\"sobrenome\":\"chacha\"}";
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
         }
     }
 }
